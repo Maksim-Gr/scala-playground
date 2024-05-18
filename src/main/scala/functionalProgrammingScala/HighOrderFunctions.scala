2317eb3484dba19bc43a056c9fb6f9710cd4a4c6
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2024-05-18 15:00:17 +0300

    chore: functional style for loop

--- src/main/scala/functionalProgrammingScala/HighOrderFunctions.scala
+++ src/main/scala/functionalProgrammingScala/HighOrderFunctions.scala
@@ -20,6 +20,15 @@ object HighOrderFunctions:
 
     go(n, 0, 1)
 
+  //Example of polymorphic functions
+  def findFirst(ss: Array[String], key: String): Int =
+    @tailrec
+    def loop(n: Int): Int =
+      if n >= ss.length then -1
+      else if ss(n) == key then n
+      else loop(n + 1)
+    loop(0)
+
   @main def printFibAndFactorial(): Unit =
     println(factorial(23))
     println(fib(123))
