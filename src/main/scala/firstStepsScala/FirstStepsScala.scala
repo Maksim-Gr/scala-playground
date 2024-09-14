cc87f0911edaccb2712c73328d29c1b51ee943a4
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2024-09-14 15:01:37 +0300

    chore: companion object

--- src/main/scala/firstStepsScala/FirstStepsScala.scala
+++ src/main/scala/firstStepsScala/FirstStepsScala.scala
@@ -1,6 +1,29 @@
 package firstStepsScala
+import scala.collection.mutable
+import scala.collection.mutable.Map
+class CheckSumAccumulator:
+  private var sum = 0
+  // all parameters inside methods are val
+  def add(b: Byte): Unit =
+    sum += b
 
-object FirstStepsScala extends App{
+  def checkSum(): Int = ~(sum & 0xff) + 1
+
+object CheckSumAccumulator:
+  private val cache = mutable.Map.empty[String, Int]
+
+  def calculate(s: String): Int =
+    if cache.contains(s) then cache(s)
+    else
+      val acc = new CheckSumAccumulator
+      for c <- s do
+        acc.add((c >> 8).toByte)
+        acc.add(c.toByte)
+      val cs = acc.checkSum()
+      cache += (s -> cs)
+      cs
+
+object FirstStepsScala extends App:
+
   @main def m(): Unit =
     println("Hello, world from Scala!!")
-}
