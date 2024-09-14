be429d702a937cbd96ce39dd7da178c03c91e8a5
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2024-09-14 15:14:26 +0300

    chore: class companion for overriding method apply

--- src/main/scala/firstStepsScala/CaseClassExample.scala
+++ src/main/scala/firstStepsScala/CaseClassExample.scala
@@ -1,8 +1,19 @@
 package firstStepsScala
 
-
 // Using case classes tells compiler to add apply/toString/hash/equals methods
-case class CaseClassExample (name: String, age: Int)
+case class CaseClassExample(name: String, age: Int):
+  def appendToName(suffix: String): CaseClassExample =
+    CaseClassExample(s"$name$suffix", age)
 
-@main def m():Unit =
+object CaseClassExample:
+  def apply(name: String, age: Int): CaseClassExample =
+    val capitalizedName =
+      if !name.isEmpty then
+        val firsChar = name.charAt(0).toUpper
+        var restOfName = name.substring(1)
+        s"$firsChar$restOfName"
+      else throw new IllegalArgumentException("Empty name")
+    new CaseClassExample(capitalizedName, age)
+
+@main def printCaseClass(): Unit =
   val value = CaseClassExample("randomName", 23)
