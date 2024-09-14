667ef4ec59a5c914fb183918fec81f98d4f3c0e6
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2024-09-14 14:23:16 +0300

    chore: implement tail function

---
+++ src/main/scala/functionalProgrammingScala/DataSharingFP.scala
@@ -0,0 +1,5 @@
+package functionalProgrammingScala
+
+object DataSharingFP {
+
+}
--- src/main/scala/functionalProgrammingScala/FunctionalDataStructures.scala
+++ src/main/scala/functionalProgrammingScala/FunctionalDataStructures.scala
@@ -1,33 +1,36 @@
 package functionalProgrammingScala
 
-object FunctionalDataStructures {
+object FunctionalDataStructures:
+  // +A covariant parameter of List
   enum List[+A]:
     case Nil
     case Cons(head: A, tail: List[A])
 
   object List:
-    def apply[A] (as: A*):List[A] =
+    // A* means variadic type: can accept 0 or more
+    def apply[A](as: A*): List[A] =
       if as.isEmpty then Nil
       else Cons(as.head, apply(as.tail*))
 
-    def sum(ints: List[Int]):Int = ints match
-      case Nil => 0
+    def sum(ints: List[Int]): Int = ints match
+      case Nil         => 0
       case Cons(x, xs) => x + sum(xs)
 
-
     def product(doubles: List[Double]): Double = doubles match
-      case Nil => 1.0
+      case Nil          => 1.0
       case Cons(0.0, _) => 0.0
-      case Cons(x, xs) => x * product(xs)
+      case Cons(x, xs)  => x * product(xs)
 
+    def tail[A](as: List[A]): List[A] = as match
+      case Cons(_, t1) => t1
+      case Nil         => sys.error("tail of the empty list")
 
-    val result = List(5, 10, 3,4, 5, 6) match
-      case Cons(x, Cons(2, Cons(4, _))) => x
-      case Nil => 42
+    val result = List(5, 10, 3, 4, 5, 6) match
+      case Cons(x, Cons(2, Cons(4, _)))          => x
+      case Nil                                   => 42
       case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-      case Cons(h, t) => h + sum(t)
-      case _ => 101
+      case Cons(h, t)                            => h + sum(t)
+      case _                                     => 101
 
-      @main def m():Unit =
-        println(result)
-}
+    @main def m(): Unit =
+      println(result)
