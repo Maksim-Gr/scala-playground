9e2a9ad9ed5f916c92e1c8d1de0c00ecd725af9f
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2024-09-15 14:10:26 +0300

    chore: method overloading

--- src/main/scala/firstStepsScala/Rational.scala
+++ src/main/scala/firstStepsScala/Rational.scala
@@ -1,14 +1,37 @@
 package firstStepsScala
 
+import scala.annotation.{tailrec, targetName}
+
 class Rational(n: Int, d: Int):
   //Check the parameters before the initial constructor
   require(d != 0)
-  val number: Int = d
-  val denom: Int = n
+
+  private val g = gcd(n.abs, d.abs)
+  val number: Int = d / g
+  val denom: Int = n / g
 
   //additional constructor
-  def this(n:Int) = this(n,1)
+  def this(n: Int) = this(n, 1)
   override def toString: String = s"$number/$denom"
 
-  def add(that: Rational): Rational =
+  @targetName("+")
+  def +(that: Rational): Rational =
     Rational(number * that.denom + that.number * denom, denom * that.denom)
+
+  //overload the methods
+  @targetName("+")
+  def +(i: Int): Rational =
+    Rational(number + i * denom, denom)
+
+  @targetName("*")
+  def *(that: Rational): Rational =
+    Rational(number * that.number, denom * that.number)
+
+  @tailrec
+  private def gcd(a: Int, b: Int): Int =
+    if b == 0 then a else gcd(b, a % b)
+
+@main def printRational(): Unit =
+  val x = Rational(1, 2)
+  val y = Rational(2, 7)
+  println(x + y)
