717ad5bbd7f2ba90fe594a990940d3b814bc01a2
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2024-09-21 13:36:43 +0300

    chore: for/while loops z

--- src/main/scala/firstStepsScala/ControlStructres.scala
+++ src/main/scala/firstStepsScala/ControlStructres.scala
@@ -1,4 +1,11 @@
 package firstStepsScala
 
+import java.io.File
+
 @main def controlStructure(args: String*): Unit =
   val fileName = if args.nonEmpty then args(0) else "default.txt"
+
+  //scala while types
+  val filesHere = (new File(".")).listFiles()
+  // generator conde
+  for file <- filesHere do println(file)
