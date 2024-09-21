ca8deb21331c9818062fa7ae78e5b9a55ef02f59
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2024-09-21 14:10:19 +0300

    chore: control structures

--- src/main/scala/firstStepsScala/ControlStructres.scala
+++ src/main/scala/firstStepsScala/ControlStructres.scala
@@ -14,3 +14,26 @@ import java.io.File
   for i <- 1 to 10 do println(s"iteration $i")
   // 5 is excluded from the range
   for i <- 1 until 5 do println(s"iteration $i")
+
+  // filters
+  for file <- filesHere if file.getName.endsWith(".sbt") do println(file)
+
+  for file <- filesHere if file.getName.endsWith(".conf") do println(file)
+
+  //nested loops
+  def fileLines(file: java.io.File) =
+    scala.io.Source.fromFile(file).getLines().toArray
+
+  def grep(pattern: String): Unit =
+    for
+      file <- filesHere if file.getName.endsWith(".scala")
+      line <- fileLines(file)
+      trimmed = line.trim
+      if trimmed.matches(pattern)
+    do println(s"$file: $trimmed")
+
+  // Creating new collections
+  val sbtFiles = for file <- filesHere if file.getName.endsWith(".sbt")
+  yield file
+
+
